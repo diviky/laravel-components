fec43fd7456040f4924805a5b2a42000cd0a22a4
SANKAR SUDA <sankar.suda@gmail.com>
2025-08-22 21:10:23 +0530

    design changes

--- resources/views/bootstrap-5/form-image.blade.php
+++ resources/views/bootstrap-5/form-image.blade.php
@@ -92,8 +92,8 @@
 
         @if ($slot->isNotEmpty())
             <!-- PREVIEW AREA -->
-            <div x-ref="preview" class="relative flex">
-                <div wire:ignore @click="change()" :class="processing && 'opacity-50 pointer-events-none'"
+            <div x-ref="preview" class="relative text-center">
+                <div @click="change()" :class="processing && 'opacity-50 pointer-events-none'"
                     class="cursor-pointer hover:scale-105 transition-all" data-bs-toggle="tooltip"
                     title="{{ $changeText }}">
                     {{ $slot }}
