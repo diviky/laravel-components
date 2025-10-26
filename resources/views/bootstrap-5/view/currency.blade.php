b517daff04a0d3b3fad7ae79712729df3024596f
SANKAR SUDA <sankar.suda@gmail.com>
2025-10-26 19:52:30 +0530

    currency display

--- resources/views/bootstrap-5/view/currency.blade.php
+++ resources/views/bootstrap-5/view/currency.blade.php
@@ -4,13 +4,18 @@
     'label' => null,
     'copy' => false,
     'settings' => [],
+    'currency' => null,
 ])
 
 @isset($value)
     <span {{ $attributes }}>
         <x-icon :name="$icon" class="me-1" />
         {!! $label !!}
-        {{ \Illuminate\Support\Number::currency($value) }}
+        @if ($currency)
+            {{ \Illuminate\Support\Number::currency($value, $currency) }}
+        @else
+            {{ \Illuminate\Support\Number::currency($value) }}
+        @endif
         @if ($copy)
             <x-icon name="copy" class="cursor-pointer" title="copy to clipboard" data-clipboard="{{ $value }}" />
         @endif
